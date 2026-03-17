c0f68de195131fa12409f851b1abd1fcc79121ec
LeoFeitosaP <leonardoperes342007@gmail.com>
2026-03-17 10:21:43 -0300

    fix: removendo erro gramatical (veicullo -> veiculo)

--- backend/src/main/java/com/VanControl/VanControl/veiculos/domain/dto/request/AtualizarStatusVeiculoRequestDto.java
+++ backend/src/main/java/com/VanControl/VanControl/veiculos/domain/dto/request/AtualizarStatusVeiculoRequestDto.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record AtualizarStatusVeiculoRequestDto(
-        @NotBlank(message = "Insira a Placa do Veicullo")
+        @NotBlank(message = "Insira a Placa do Veiculo")
         @Pattern(regexp = "^[A-Z]{3}-[0-9][A-Z0-9][0-9]{2}$", message = "A placa deve seguir estar no formato Mercosul ou antigo")
         String placa,
         @NotBlank(message = "Insira o status do veiculo")
