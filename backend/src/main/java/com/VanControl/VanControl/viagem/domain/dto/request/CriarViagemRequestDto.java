0039b10f5a8cf53359cea59a9fd3fc444d5644f5
Leonardo Feitosa Peres <leonardoperes342007@gmail.com>
2026-04-10 18:37:20 -0300

    feat: Entidade viagem adicionada

---
+++ backend/src/main/java/com/VanControl/VanControl/viagem/domain/dto/request/CriarViagemRequestDto.java
@@ -0,0 +1,10 @@
+package com.VanControl.VanControl.viagem.domain.dto.request;
+
+import java.util.UUID;
+
+public record CriarViagemRequestDto(
+        string
+
+
+) {
+}
---
+++ backend/src/main/java/com/VanControl/VanControl/viagem/domain/entity/Viagem.java
@@ -0,0 +1,40 @@
+package com.VanControl.VanControl.viagem.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Viagem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private UUID rotaId;
+    private UUID veiculoId;
+    private UUID motoristaId;
+
+    private String dataViagem;
+    private String horarioSaidaPrevisto;
+    private String horarioChegadaPrevisto;
+    private boolean viagemConcluida;
+
+    public Viagem(UUID rotaId, UUID veiculoId, UUID motoristaId, String dataViagem, String horarioSaidaPrevisto, String horarioChegadaPrevisto) {
+        this.rotaId = rotaId;
+        this.veiculoId = veiculoId;
+        this.motoristaId = motoristaId;
+        this.dataViagem = dataViagem;
+        this.horarioSaidaPrevisto = horarioSaidaPrevisto;
+        this.horarioChegadaPrevisto = horarioChegadaPrevisto;
+    }
+}
