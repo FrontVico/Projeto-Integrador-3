bbdaa736948f2166b99d150a2050dd9ad693bd53
Leonardo Feitosa Peres <leonardoperes342007@gmail.com>
2026-06-17 21:39:59 -0300

    fix: VanControlApplicationTests não carregava o db

--- backend/src/test/java/com/VanControl/VanControl/VanControlApplicationTests.java
+++ backend/src/test/java/com/VanControl/VanControl/VanControlApplicationTests.java
@@ -2,8 +2,10 @@ package com.VanControl.VanControl;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class VanControlApplicationTests {
 
 	@Test
